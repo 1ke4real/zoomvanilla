21bc15fb8c7af9dd654abcf370f781f72f6342f5
Mike Leman <mikeeleman@gmail.com>
2022-10-14 19:16:19 +0200

    setting up the zoom effect

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ let cont = document.querySelector('#container')
 let img = document.querySelector('img')
 
 
-
 cont.addEventListener( 'mousemove', (e) => {
+
+    // mouse position recovery
+
     let x = e.clientX - e.target.offsetLeft
     let y = e.clientY - e.target.offsetTop
 
     console.log(x, y)
+
+    // zoom effect
+
+    img.style.transformOrigin = ` ${x}px ${y}px`
+    img.style.transform = 'scale(2)'
 });
+
+// off effect 
+
+cont.addEventListener( 'mouseleave', () => {
+    img.style.transformOrigin = 'center'
+    img.style.transform = 'scale(1)'
+})
